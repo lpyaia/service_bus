9e2d737af3d833541d9d1abe9b82f4b6a4f0df53
Lucas Pyaia França <brhbs0551@ambev.com.br>
2021-08-26 19:35:22 -0300

    simple topic publisher/subscriber

---
+++ ServiceBus/Topic.Consumer/Program.cs
@@ -0,0 +1,56 @@
+﻿using Azure.Messaging.ServiceBus;
+using System;
+using System.Threading.Tasks;
+
+namespace Topic.Consumer
+{
+    internal class Program
+    {
+        private static string _connectionString = "Endpoint=sb://lpyaia.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=rda4pc2ieQ/ucapQ8GpybN6OjeHnR673otOeELVmck8=";
+        private static string _topicName = "topic";
+        private static string _subscriptionName = "S3";
+        private static ServiceBusClient _client;
+        private static ServiceBusProcessor _processor;
+
+        private static async Task MessageHandler(ProcessMessageEventArgs args)
+        {
+            string body = args.Message.Body.ToString();
+            Console.WriteLine($"Received: {body} from subscription: {_subscriptionName}");
+
+            await args.CompleteMessageAsync(args.Message);
+        }
+
+        private static Task ErrorHandler(ProcessErrorEventArgs args)
+        {
+            Console.WriteLine(args.Exception.ToString());
+            return Task.CompletedTask;
+        }
+
+        private static async Task Main(string[] args)
+        {
+            _client = new ServiceBusClient(_connectionString);
+
+            var options = new ServiceBusProcessorOptions()
+            {
+                MaxConcurrentCalls = 3
+            };
+
+            _processor = _client.CreateProcessor(_topicName, _subscriptionName, options);
+
+            _processor.ProcessMessageAsync += MessageHandler;
+            _processor.ProcessErrorAsync += ErrorHandler;
+
+            await _processor.StartProcessingAsync();
+
+            Console.WriteLine("Wait for a minute and then press any key to end the processing");
+            Console.ReadKey();
+
+            // stop processing
+            Console.WriteLine("\nStopping the receiver...");
+
+            await _processor.StopProcessingAsync();
+
+            Console.WriteLine("Stopped receiving messages");
+        }
+    }
+}
---
+++ ServiceBus/Topic.Publisher/Program.cs
@@ -0,0 +1,45 @@
+﻿using Azure.Messaging.ServiceBus;
+using System;
+using System.Threading.Tasks;
+
+namespace Topic.Publisher
+{
+    internal class Program
+    {
+        private static string _connectionString = "Endpoint=sb://lpyaia.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=rda4pc2ieQ/ucapQ8GpybN6OjeHnR673otOeELVmck8=";
+        private static string _topicName = "topic";
+        private static ServiceBusClient _client;
+        private static ServiceBusSender _sender;
+        private const int _numOfMessages = 15;
+
+        private static async Task Main(string[] args)
+        {
+            _client = new ServiceBusClient(_connectionString);
+            _sender = _client.CreateSender(_topicName);
+
+            using ServiceBusMessageBatch messageBatch = await _sender.CreateMessageBatchAsync();
+
+            for (int i = 1; i <= _numOfMessages; i++)
+            {
+                if (!messageBatch.TryAddMessage(new ServiceBusMessage($"Message {i}")))
+                {
+                    throw new Exception($"\nThe message {i} is too large to fit in the batch");
+                }
+            }
+
+            try
+            {
+                await _sender.SendMessagesAsync(messageBatch);
+                Console.WriteLine($"A batch of {_numOfMessages} messages has been published to the topic.");
+            }
+            finally
+            {
+                await _sender.DisposeAsync();
+                await _client.DisposeAsync();
+            }
+
+            Console.WriteLine("Press any key to end the application");
+            Console.ReadKey();
+        }
+    }
+}
